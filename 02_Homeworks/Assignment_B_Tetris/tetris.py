5810f395a8c25efc45db06b095bed22039efaeb4
FelixW <54879971+FelixWaldschock@users.noreply.github.com>
2024-04-15 20:53:09 +0200

    update on Assignment B

--- 02_Homeworks/Assignment_B_Tetris/tetris.py
+++ 02_Homeworks/Assignment_B_Tetris/tetris.py
@@ -24,14 +24,14 @@ param_set=PARAM_TASK1a
 # param_set=PARAM_TASK1b
 # param_set=PARAM_TASK1c
 # param_set=PARAM_TASK1d
-param_set=PARAM_TASK2a
+# param_set=PARAM_TASK2a
 #param_set=PARAM_TASK2b
 
 # Use files to evaluate strategy
 # If you change 'strategy_file' to the location of a file containing a stored Q-table or Q-network, you can evaluate the success of the found strategy
 if param_set==PARAM_TASK1a:
     strategy_file=''
-    # strategy_file='logfiles/Q-table_60.npy'
+    strategy_file='logfiles/bestQ-table_4.npy'
 elif param_set==PARAM_TASK1b:
     strategy_file=''
 elif param_set==PARAM_TASK1c:
